3ca43db584dc9c05af99abedb501ed7d2a414cdc
Ilansezhiyan Sivanandam <elan116@kablan1.servers.lowrisc.org>
2026-09-22 15:03:47 +0100

    fixes for kmac regression fails (1. scoreboard synchronization issue 2.contraint synchronization issue 3.dependent pre_randomize issue 4. Keymgr masked mode issue with scoareboard and 5. coverage null pointer issues) and implement rsp_ready handling with different back pressure policies

--- hw/dv/sv/kmac_app_agent/kmac_app_host_agent.sv
+++ hw/dv/sv/kmac_app_agent/kmac_app_host_agent.sv
@@ -11,6 +11,7 @@ class kmac_app_host_agent extends dv_base_agent #(.CFG_T       (kmac_app_agent_c
 
   extern function new(string name, uvm_component parent);
   extern function void build_phase(uvm_phase phase);
+  extern function void connect_phase(uvm_phase phase);
 endclass
 
 function kmac_app_host_agent::new(string name, uvm_component parent);
@@ -27,4 +28,29 @@ function void kmac_app_host_agent::build_phase(uvm_phase phase);
 
   // Configure the interface to match the agent.
   cfg.vif.if_mode = cfg.is_active ? Host : Monitor;
+
+  if (cfg.is_active && cfg.rsp_ready_policy == null) begin
+    string policy_name = "always";
+    void'($value$plusargs("kmac_app_rsp_ready_policy=%0s", policy_name));
+    case (policy_name)
+      "always": cfg.rsp_ready_policy =
+          kmac_app_rsp_ready_always_policy::type_id::create("rsp_ready_policy");
+      "always_with_dip": cfg.rsp_ready_policy =
+          kmac_app_rsp_ready_always_with_dip_policy::type_id::create("rsp_ready_policy");
+      "random": cfg.rsp_ready_policy =
+          kmac_app_rsp_ready_random_policy::type_id::create("rsp_ready_policy");
+      "when_valid": cfg.rsp_ready_policy =
+          kmac_app_rsp_ready_when_valid_policy::type_id::create("rsp_ready_policy");
+      default: `uvm_fatal(`gfn, $sformatf(
+          "Unknown +kmac_app_rsp_ready_policy value: %0s", policy_name))
+    endcase
+  end
+endfunction
+
+function void kmac_app_host_agent::connect_phase(uvm_phase phase);
+  super.connect_phase(phase);
+
+  if (cfg.is_active) begin
+    driver.m_rsp_port.connect(sequencer.m_rsp_fifo.analysis_export);
+  end
 endfunction
